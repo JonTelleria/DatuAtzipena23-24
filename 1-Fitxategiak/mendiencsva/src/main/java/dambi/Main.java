e06432e3ac3aa8e6a4bf89f7cf7e31e0f98c8bcc
JonTelleria <telleria.jon@uni.eus>
2023-09-19 13:10:34 +0200

    Mendiencsva amaituta

--- 1-Fitxategiak/mendiencsva/src/main/java/dambi/Main.java
+++ 1-Fitxategiak/mendiencsva/src/main/java/dambi/Main.java
@@ -3,6 +3,7 @@ package dambi;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -20,7 +21,7 @@ public class Main {
             System.out.println("====================================");
             System.out.println("1.- Mendien zerrenda ikusi (taula formatuan)");
             System.out.println("2.- Mendirik altuena bistaratu");
-            System.out.println("3.- Mendiak esportatu (Araba.csv, Bizkaia.csv, Gipuzkoa.csv)");
+            System.out.println("3.- Mendiak esportatu (Araba.csv, Bizkaia.csv, Gipuzkoa.csv, Nafarroa.csv)");
             System.out.println("4.- ...");
             System.out.println("5.- Irten");
             System.out.println("");
@@ -33,6 +34,10 @@ public class Main {
                 case 2:
                     mendiAltuena();
                     break;
+                case 3:
+                    importatuCsv();
+                    break;
+
 
                 case 5:
                     System.out.println("Eskerrik asko programa hau erabiltzeagatik.");
@@ -92,6 +97,62 @@ public class Main {
             if (inputStream != null) {
                 inputStream.close();
             }
+        }
+    }
+    public static void importatuCsv() throws IOException{
+        String l;
+        BufferedReader inputStream = null;
+        PrintWriter outputStreamGip = null;
+        PrintWriter outputStreamBizk = null;
+        PrintWriter outputStreamAra = null;
+        PrintWriter outputStreamNaf = null;
+         try {
+            inputStream = new BufferedReader(new FileReader("Mendiak.csv"));
+            outputStreamGip = new PrintWriter(new FileWriter("Gipuzkoa.csv"));
+            outputStreamBizk = new PrintWriter(new FileWriter("Bizkaia.csv"));
+            outputStreamAra = new PrintWriter(new FileWriter("Araba.csv"));
+            outputStreamNaf = new PrintWriter(new FileWriter("Nafarroa.csv"));
+
+            while ((l = inputStream.readLine()) != null) {
+               
+                if (l.contains("PROBINTZIA")){
+                    outputStreamGip.println(l);
+                    outputStreamAra.println(l);
+                    outputStreamBizk.println(l);
+                    outputStreamNaf.println(l);
+                }
+                else if(l.contains("Gipuzkoa")){
+                    outputStreamGip.println(l);
+                }
+                else if(l.contains("Bizkaia")){
+                    outputStreamBizk.println(l);
+                }
+                else if(l.contains("Araba")){
+                    outputStreamAra.println(l);
+                }
+                else if(l.contains("Nafarroa")){
+                    outputStreamNaf.println(l);
+                }
+                
+
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStreamGip != null) {
+                outputStreamGip.close();
+            }
+            if (outputStreamBizk != null) {
+                outputStreamBizk.close();
+            }
+            if (outputStreamAra != null) {
+                outputStreamAra.close();
+            }
+            if (outputStreamNaf != null) {
+                outputStreamNaf.close();
+            }
+
         }
     }
 }
